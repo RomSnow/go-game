414ed20734a31dc267330b21e5eede1e694d851f
RomSnow <ivashkin.roma2001@yandex.ru>
2020-09-14 18:28:27 +0500

    Корректирование тестов

--- Tests/game_manager_tests.py
+++ Tests/game_manager_tests.py
@@ -4,8 +4,12 @@ import game_core.game_manager as gm
 
 
 class GameManagerTests(unittest.TestCase):
+    white_player = gm.player.Player(gm.stones.WhiteStone)
+    black_player = gm.player.Player(gm.stones.BlackStone)
+
     def test_make_move(self):
-        game = gm.Game(gm.field.FieldParams(9, 9))
+        game = gm.Game(gm.field.FieldParams(9, 9),
+                       self.white_player, self.black_player)
         first_player = game.current_player
         game.make_move('pass', 0, 0)
         self.assertNotEqual(game.current_player, first_player)
@@ -14,7 +18,8 @@ class GameManagerTests(unittest.TestCase):
         self.assertRaises(gm.exc.IncorrectMove, game.make_move, 'asf', 0, 0)
 
     def test_end_game(self):
-        game = gm.Game(gm.field.FieldParams(9, 9))
+        game = gm.Game(gm.field.FieldParams(9, 9),
+                       self.white_player, self.black_player)
         game.make_move('pass')
         game.make_move('pass')
         self.assertFalse(game.game_is_on)
