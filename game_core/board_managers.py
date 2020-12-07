5b6806c083381971dd8b28f0e4cbda83812e875c
RomSnow <ivashkin.roma2001@yandex.ru>
2020-12-07 17:12:51 +0500

    ScoreBoard

---
+++ game_core/board_managers.py
@@ -0,0 +1,66 @@
+from abc import abstractmethod
+import re
+
+
+class BoardManager:
+    @abstractmethod
+    def get_board_name(self):
+        """Имя"""
+
+    @abstractmethod
+    def get_board_str(self) -> str:
+        """Текс таблицы"""
+
+
+class ScoreBoardManager(BoardManager):
+    def __init__(self, score_file_name: str):
+        self._file_name = score_file_name
+        self.score_table = dict()
+        self._read_board()
+
+    def get_board_name(self):
+        return 'Таблица рейтинга'
+
+    def get_board_str(self) -> str:
+        with open(self._file_name, 'r') as file:
+            data = file.read()
+
+        return data
+
+    def _read_board(self):
+        with open(self._file_name, 'r') as file:
+            data = file.read()
+
+        lines = data.split('\n')
+
+        for line in lines:
+            data = re.findall(r'(\w+)', line)
+            if not data:
+                break
+            pos, name, score = int(data[0]), data[1], int(data[2])
+            self.score_table[name] = score
+
+    def set_player_score(self, player_name: str, score: int):
+        if player_name in self.score_table:
+            self.score_table[player_name] = self.score_table[player_name] + score
+        else:
+            self.score_table[player_name] = score
+
+        self._write_in_file()
+
+    def _write_in_file(self):
+        lines = list()
+        i = 1
+        for name, score in sorted(self.score_table.items(),
+                                  key=lambda _t: _t[1],
+                                  reverse=True):
+            pos_str = f'{i}'.ljust(8, ' ')
+            name_str = f'{name}'.ljust(12, ' ')
+            score_str = f'{score}'.ljust(9, ' ')
+
+            line_str = f'{pos_str}{" " * 40}{name_str}{" " * 25}{score_str}\n'
+            lines.append(line_str)
+            i += 1
+
+        with open(self._file_name, 'w') as file:
+            file.writelines(lines)
---
+++ graphics/board_win.py
@@ -0,0 +1,32 @@
+import PyQt5.QtWidgets as qtw
+
+from game_core.board_managers import BoardManager
+
+
+class ScoreBoardWindow(qtw.QWidget):
+    def __init__(self, board_manager: BoardManager):
+        super().__init__()
+        self._board = board_manager
+        self._init_ui()
+        self.show()
+
+    def _init_ui(self):
+        self.setFixedSize(480, 600)
+        self.setWindowTitle(self._board.get_board_name())
+
+        main_label = qtw.QVBoxLayout()
+        text_label = qtw.QLabel()
+        scroll_area = qtw.QScrollArea()
+
+        text_label.setText(self._board.get_board_str())
+        text_label.setSizePolicy(qtw.QSizePolicy.Preferred,
+                                 qtw.QSizePolicy.Preferred)
+        scroll_area.setWidget(text_label)
+
+        ok_button = qtw.QPushButton('Закрыть')
+        ok_button.clicked.connect(self.close)
+
+        main_label.addWidget(scroll_area)
+        main_label.addWidget(ok_button)
+
+        self.setLayout(main_label)
